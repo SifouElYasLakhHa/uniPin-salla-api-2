17b5151281e8a162b8399ef05aecdce345d1aca0
SifouElYasLakhHa <69309293+SifouElYasLakhHa@users.noreply.github.com>
2022-09-08 02:33:56 +0100

    Update

--- src/controllers/pages/loginController.js
+++ src/controllers/pages/loginController.js
@@ -1,24 +1,10 @@
-const db = require('./../../moduls/index.js');
-
 exports.loginPage = async (req, res) => {
   try {
-    db.Users.find({}, (e, users) => {
-      if(e) {
-        return res.status(500).json({
-            error: true,
-            type: 'Find user',
-            error: {
-              status: true,
-              message: 'please contact developer for fixing issues',
-            }
-          });
-      }
-      return res.render('pages/loginPage', {
-        mainTitle: process.env.APP_NAME,
-        host: process.env.HOST,
-        subTitle: 'تسجيل الدخول',
-      });
-    })
+    return res.render('pages/loginPage', {
+      mainTitle: process.env.APP_NAME,
+      host: process.env.HOST,
+      subTitle: 'تسجيل الدخول',
+    });
   } catch (e) {
     console.log(e)
     return res.status(500).json({
